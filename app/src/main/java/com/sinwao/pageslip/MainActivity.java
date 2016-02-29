c8dfb2909439ec9eb65ea26302d91366fa264c5f
artcool <710760186@qq.com>
2016-03-01 00:48:24 +0800

    Initial commit

---
+++ app/src/main/java/com/sinwao/pageslip/MainActivity.java
@@ -0,0 +1,18 @@
+package com.sinwao.pageslip;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * TabLayout和ViewPager简单实现页卡的滑动
+ * 作者：artcool
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
